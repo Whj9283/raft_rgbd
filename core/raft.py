8ec69a67ccdf1e91071348d6809cd8ce3f001eac
whj555 <1621370123@qq.com>
2024-05-03 15:40:09 +0800

    修改bug

--- core/raft.py
+++ core/raft.py
@@ -15,8 +15,10 @@ except:
     class autocast:
         def __init__(self, enabled):
             pass
+
         def __enter__(self):
             pass
+
         def __exit__(self, *args):
             pass
 
@@ -31,7 +33,7 @@ class RAFT(nn.Module):
             self.context_dim = cdim = 64
             args.corr_levels = 4
             args.corr_radius = 3
-        
+
         else:
             self.hidden_dim = hdim = 128
             self.context_dim = cdim = 128
@@ -46,13 +48,13 @@ class RAFT(nn.Module):
 
         # feature network, context network, and update block
         if args.small:
-            self.fnet = SmallEncoder(output_dim=128, norm_fn='instance', dropout=args.dropout)        
-            self.cnet = SmallEncoder(output_dim=hdim+cdim, norm_fn='none', dropout=args.dropout)
+            self.fnet = SmallEncoder(output_dim=128, norm_fn='instance', dropout=args.dropout)
+            self.cnet = SmallEncoder(output_dim=hdim + cdim, norm_fn='none', dropout=args.dropout)
             self.update_block = SmallUpdateBlock(self.args, hidden_dim=hdim)
 
         else:
-            self.fnet = BasicEncoder(output_dim=256, norm_fn='instance', dropout=args.dropout)        
-            self.cnet = BasicEncoder(output_dim=hdim+cdim, norm_fn='batch', dropout=args.dropout)
+            self.fnet = BasicEncoder(output_dim=256, norm_fn='instance', dropout=args.dropout)
+            self.cnet = BasicEncoder(output_dim=hdim + cdim, norm_fn='batch', dropout=args.dropout)
             self.update_block = BasicUpdateBlock(self.args, hidden_dim=hdim)
 
     def freeze_bn(self):
@@ -63,8 +65,8 @@ class RAFT(nn.Module):
     def initialize_flow(self, img):
         """ Flow is represented as difference between two coordinate grids flow = coords1 - coords0"""
         N, C, H, W = img.shape
-        coords0 = coords_grid(N, H//8, W//8, device=img.device)
-        coords1 = coords_grid(N, H//8, W//8, device=img.device)
+        coords0 = coords_grid(N, H // 8, W // 8, device=img.device)
+        coords1 = coords_grid(N, H // 8, W // 8, device=img.device)
 
         # optical flow computed as difference: flow = coords1 - coords0
         return coords0, coords1
@@ -75,22 +77,22 @@ class RAFT(nn.Module):
         mask = mask.view(N, 1, 9, 8, 8, H, W)
         mask = torch.softmax(mask, dim=2)
 
-        up_flow = F.unfold(8 * flow, [3,3], padding=1)
+        up_flow = F.unfold(8 * flow, [3, 3], padding=1)
         up_flow = up_flow.view(N, 2, 9, 1, 1, H, W)
 
         up_flow = torch.sum(mask * up_flow, dim=2)
         up_flow = up_flow.permute(0, 1, 4, 2, 5, 3)
-        return up_flow.reshape(N, 2, 8*H, 8*W)
+        return up_flow.reshape(N, 2, 8 * H, 8 * W)
 
-
-    def forward(self, image1, depth1, image2, depth2,iters=12, flow_init=None, upsample=True, test_mode=False):
+    def forward(self, image1, depth1, image2, depth2, iters=12, flow_init=None, upsample=True, test_mode=False):
         """ Estimate optical flow between pair of frames """
-        #输入归一化
+        # 输入归一化
         image1 = 2 * (image1 / 255.0) - 1.0
         image2 = 2 * (image2 / 255.0) - 1.0
         # 深度取倒数
         depth1_inv = torch.where(depth1 != 0, torch.reciprocal(depth1), torch.tensor(0.0).to(depth1.device))
         depth2_inv = torch.where(depth2 != 0, torch.reciprocal(depth2), torch.tensor(0.0).to(depth2.device))
+        # 此时image1:torch.Size([12, 3, 368, 768])。depth1_inv:torch.Size([12, 1, 368, 768])
 
         device = torch.device('cuda:0')  # 指定设备为第一个GPU
         image1 = image1.to(device)  # 将image1迁移到GPU
@@ -98,45 +100,61 @@ class RAFT(nn.Module):
         image2 = image2.to(device)
         depth2_inv = depth2_inv.to(device)
         # 现在两个张量都在GPU上，可以安全地进行拼接操作
-        depth1_inv = depth1_inv.unsqueeze(1)  # 增加一个维度，使其变为4维张量
-        depth2_inv = depth2_inv.unsqueeze(1)  # 增加一个维度，使其变为4维张量
+        # if depth1_inv.dim() == 3:
+        #     # 增加一个维度，使其变为4维张量
+        #     depth1_inv = depth1_inv.unsqueeze(1)
+        # if depth2_inv.dim() == 3:
+        #     depth1_inv = depth2_inv.unsqueeze(1)
+        current_shape = image1.size()  # 这将返回一个类似 torch.Size([BATCH, CHANNELS, H, W]) 的对象
+        batch_size, channels, height, width = current_shape
+        depth1_inv = depth1_inv.reshape(batch_size, -1, height, width)
+        depth2_inv = depth2_inv.reshape(batch_size, -1, height, width)
+
+        # print("image1", end=':')
+        # print(image1.size())
+        # print("depth1_inv", end=':')
+        # print(depth1_inv.size())
+
         # 将 depth0 和 depth1 加入到新增的通道中
         image1 = torch.cat((image1, depth1_inv), dim=1)  # [B, 4, H, W]
         image2 = torch.cat((image2, depth2_inv), dim=1)  # [B, 4, H, W]
+
+        # print("image11", end=':')
+        # print(image1.size())
 
         image1 = image1.contiguous()
         image2 = image2.contiguous()
 
         hdim = self.hidden_dim
         cdim = self.context_dim
-        #特征网络
+        # 特征网络
         # run the feature network
         with autocast(enabled=self.args.mixed_precision):
-            fmap1, fmap2 = self.fnet([image1, image2])        
-        #相关性计算
+            fmap1, fmap2 = self.fnet([image1, image2])
+            # 相关性计算
         fmap1 = fmap1.float()
         fmap2 = fmap2.float()
         if self.args.alternate_corr:
             corr_fn = AlternateCorrBlock(fmap1, fmap2, radius=self.args.corr_radius)
         else:
             corr_fn = CorrBlock(fmap1, fmap2, radius=self.args.corr_radius)
-        #上下文网络
+        # 上下文网络
         # run the context network
         with autocast(enabled=self.args.mixed_precision):
             cnet = self.cnet(image1)
             net, inp = torch.split(cnet, [hdim, cdim], dim=1)
             net = torch.tanh(net)
             inp = torch.relu(inp)
-        #初始化光流
+        # 初始化光流
         coords0, coords1 = self.initialize_flow(image1)
 
         if flow_init is not None:
             coords1 = coords1 + flow_init
-        #迭代更新块
+        # 迭代更新块
         flow_predictions = []
         for itr in range(iters):
             coords1 = coords1.detach()
-            corr = corr_fn(coords1) # index correlation volume
+            corr = corr_fn(coords1)  # index correlation volume
 
             flow = coords1 - coords0
             with autocast(enabled=self.args.mixed_precision):
@@ -144,16 +162,16 @@ class RAFT(nn.Module):
 
             # F(t+1) = F(t) + \Delta(t)
             coords1 = coords1 + delta_flow
-            #光流上采样
+            # 光流上采样
             # upsample predictions
             if up_mask is None:
                 flow_up = upflow8(coords1 - coords0)
             else:
                 flow_up = self.upsample_flow(coords1 - coords0, up_mask)
-            
+
             flow_predictions.append(flow_up)
 
         if test_mode:
             return coords1 - coords0, flow_up
-            
+
         return flow_predictions
